f0bca6821bfa7fe34ce2485dab13115cd12abffa
NileshVS <50776400+NileshVS@users.noreply.github.com>
2019-07-01 16:11:19 +0530

    Updated version
    Added images for all category,
    use of ifSwitch case

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,35 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {likes} from './likeButton.component';
-import { objParentClass } from './objectParent.component';
-import {objChildClass} from './objectChild.component';
-import {propertyComponent} from './property.component';
-import {TaskComponent} from './task.component';
-import {TaskOneComponent} from './taskone.component';
-import {class1} from './app.class1';
-import {class2} from './app.class2';
-import {store} from './store.component';
-import {tvComponent} from './television.component';
-import {ac} from './ac.component';
-import {laptopComponent} from './laptop.component';
-import {phoneComponent} from './phone.component';
+import { store } from './store.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    likes,
-    objParentClass,
-    objChildClass,
-    propertyComponent,
-    TaskComponent,
-    TaskOneComponent,
-    class1,
-    class2,
-    store,
-    tvComponent,
-    ac,
-    laptopComponent,
-    phoneComponent
+    store
   ],
   imports: [
     BrowserModule
--- app/store.component.ts
+++ app/store.component.ts
@@ -1,41 +1,75 @@
 import {Component} from '@angular/core';
 
 @Component({
-selector:'app-store',
-template:`
-<div type='container' class='leftPanel'>
-<app-television [tvUrls]="tvUrls"
-(tvDetails)=printTvDetails($event)
-></app-television>
-<app-ac [acUrls]="acUrls"
-(acDetails)=printAcDetails($event)
-></app-ac>
-<app-phone [phoneUrls]="phoneUrls"
-(phoneDetails)=printPhoneDetails($event)
-></app-phone>
 
-<app-laptop [laptopUrls]="laptopUrls"
-(laptopDetails)=printLaptopDetails($event)
-></app-laptop>
-
-</div>
-`
+    selector:'app-store',
+    template:
+    `
+        <div class='container'>
+            <h3>Online Store</h3>
+        </div>
+        <div align='center' class='container'>
+            <button 
+                (click)="selected='mobiles'"
+            class='btn btn-danger'>Mobile phones</button> 
+            <button 
+                (click)="selected='laptop'"
+            class='btn btn-danger'>Laptops</button>
+            <button 
+                (click)="selected='ac'"
+            class='btn btn-danger'>Air-Conditioners</button>
+            <button 
+                (click)="selected='tv'"
+            class='btn btn-danger'>Televisions</button>
+            <button
+                (click)="selected='clear'"
+            class="btn btn-default">Clear all</button><br>
+        </div>
+        <div [ngSwitch]="selected"  class='container'>
+            <span *ngSwitchCase="'mobiles'">
+                <table border=1>
+                    <td *ngFor="let url of mobiles">
+                        <tr><img src="{{url}}"></tr>
+                    </td>
+                </table>    
+            </span>
+            <span *ngSwitchCase="'tv'">
+                <table border=1 >
+                    <td *ngFor="let url of tv">
+                        <tr><img src="{{url}}"></tr>
+                    </td>
+                </table>    
+            </span>
+            <span *ngSwitchCase="'laptop'">
+                <table border=1 >
+                    <td *ngFor="let url of laptop">
+                        <tr><img src="{{url}}"></tr>
+                    </td>
+                </table>    
+            </span>
+            <span *ngSwitchCase="'ac'">
+                <table border=1 >
+                    <td *ngFor="let url of ac">
+                        <tr><img src="{{url}}"></tr>
+                    </td>
+                </table>    
+            </span>
+            <span *ngSwitchCase="'clear'">
+                <br><p align="center">Cleared!!!</p>
+            </span>
+            <span *ngSwitchDefault>
+                <h3 align='center'>Please select a category</h3>
+            </span>
+            
+        </div> 
+    `
 })
+
 export class store{
-    public tvUrls=[];
-    public acUrls=[];
-    public laptopUrls=[];
-    public phoneUrls=[];
-    printTvDetails(data){
-        this.tvUrls.push(data);
-    }
-    printAcDetails(data){
-        this.acUrls.push(data);
-    }
-    printLaptopDetails(data){
-        this.acUrls.push(data);
-    }
-    printPhoneDetails(data){
-        this.phoneUrls.push(data);
-    }
+    selected;
+    public mobiles=['./assets/images/mobiles/1.jpg','./assets/images/mobiles/2.jpg','./assets/images/mobiles/3.jpg','./assets/images/mobiles/4.jpg','./assets/images/mobiles/5.jpg','./assets/images/mobiles/6.jpg'];
+    public tv=['./assets/images/tv/1.jpg','./assets/images/tv/2.jpg','./assets/images/tv/3.jpg','./assets/images/tv/4.jpg','./assets/images/tv/5.jpg','./assets/images/tv/6.jpg'];
+    public laptop=['./assets/images/laptop/1.jpg','./assets/images/laptop/2.jpg','./assets/images/laptop/3.jpg','./assets/images/laptop/4.jpg','./assets/images/laptop/5.jpg','./assets/images/laptop/6.jpg'];
+    public ac=['./assets/images/ac/1.jpg','./assets/images/ac/2.jpg','./assets/images/ac/3.jpg','./assets/images/ac/4.jpg','./assets/images/ac/5.jpg','./assets/images/ac/6.jpg'];
+    constructor(){}
 }
